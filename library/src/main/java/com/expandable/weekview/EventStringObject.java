067a7c7e3ee35686ea173d392face1fc85e4c158
ThejanThrimanna <thejan.dthrimanna@gmail.com>
2020-04-22 23:06:52 +0530

    Minnor change

--- library/src/main/java/com/expandable/weekview/EventStringObject.java
+++ library/src/main/java/com/expandable/weekview/EventStringObject.java
@@ -4,9 +4,18 @@ package com.expandable.weekview;
  * Created by thejanthrimanna on 2020-04-20.
  */
 public class EventStringObject {
+    private String id;
     private String text;
     private Boolean isBold;
     private Boolean isShow;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getText() {
         return text;
