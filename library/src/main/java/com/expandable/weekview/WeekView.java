d32bcce1b1890b28f0ed433248b23dabda906460
ThejanThrimanna <thejan.dthrimanna@gmail.com>
2020-10-11 09:02:16 +0530

    Width change fix

--- library/src/main/java/com/expandable/weekview/WeekView.java
+++ library/src/main/java/com/expandable/weekview/WeekView.java
@@ -829,7 +829,7 @@ public class WeekView extends View {
 							right > mHeaderColumnWidth &&
 							bottom > mHeaderHeight + mHeaderRowPadding * 2 + mTimeTextHeight / 2 + mHeaderMarginBottom
 					) {
-						mEventRects.get(i).rectF = new RectF(left, top, 200, bottom);
+						mEventRects.get(i).rectF = new RectF(left, top, right, bottom);
 						mEventBackgroundPaint.setColor(mEventRects.get(i).event.getColor() == 0 ? mDefaultEventColor : mEventRects.get(i).event.getColor());
 						canvas.drawRoundRect(mEventRects.get(i).rectF, mEventCornerRadius, mEventCornerRadius, mEventBackgroundPaint);
 						drawEventTitle(mEventRects.get(i).event, mEventRects.get(i).rectF, canvas, top, left);
